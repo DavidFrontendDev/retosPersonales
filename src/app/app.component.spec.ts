580db43a054f00c988dae286972d4afd7e3ee621
DavidFrontendDev <davidfrontenddev@gmail.com>
2025-03-21 18:39:48 +0100

    TailwindCSS instalado y cambio a CSS

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'retosPersonales' title`, () => {
+  it(`should have the 'retosPersonale' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('retosPersonales');
+    expect(app.title).toEqual('retosPersonale');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, retosPersonales');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, retosPersonale');
   });
 });
